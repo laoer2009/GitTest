bc3039d74cc6e9f04f1156f10ec33152595ba442
laoer <chunli312@sohu.com>
2019-05-06 23:27:00 +0800

    增加项目

---
+++ VS2017/VS2017/小团队构建大网站/图片处理问题/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 图片处理问题
+{
+    public class Class1
+    {
+    }
+}
